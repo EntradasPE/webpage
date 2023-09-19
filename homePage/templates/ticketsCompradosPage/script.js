fe51ff16ab945c3ca6e9fa446d5614f23605108f
EntradasPE <144493279+EntradasPE@users.noreply.github.com>
2023-09-19 11:51:22 -0500

    Add files via upload

---
+++ homePage/templates/ticketsCompradosPage/script.js
@@ -0,0 +1,41 @@
+window.onbeforeunload = function () {
+    window.location.reload(true);
+};
+
+
+
+ScrollReveal().reveal('.containerTicketsDescargar', { delay: 1000 });
+ScrollReveal().reveal('.seleccionarFechaBanner', { delay: 1000 });
+
+function download(cadena) {
+    console.log(cadena.toString())
+}
+
+
+function download2(cadena, contador) {
+    var url = document.getElementById('download' + cadena.toString()).getAttribute('data-url');
+    var iframe = document.createElement('iframe');
+    iframe.style.display = 'none';
+    document.body.appendChild(iframe);
+    var iframeDocument = iframe.contentWindow.document;
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            iframeDocument.open();
+            iframeDocument.write(xhr.responseText);
+            iframeDocument.close();
+            iframeDocument.querySelector('style').textContent += '@page { size: 1600px 805px; }';
+            var element = iframeDocument.body;
+            var opt = {
+                margin: 1,
+                filename: cadena + ".pdf",
+                image: { type: 'jpeg', quality: 0.98 },
+                html2canvas: { scale: 1 },
+                jsPDF: { unit: 'px', format: [1600, 800], orientation: 'landscape' }
+            };
+            html2pdf().set(opt).from(element).save();
+        }
+    };
+    xhr.send();
+}
